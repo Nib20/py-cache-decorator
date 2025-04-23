097b97588cf64d3df7cfca4f52ff47d5ca147619
Nadiia Bohdan <n.bogdan202@gmail.com>
2025-04-23 20:08:33 +0300

    some fixes

--- app/main.py
+++ app/main.py
@@ -10,9 +10,9 @@ def cache(func: Callable) -> Callable:
         if args in cache_memory:
             print("Getting from cache")
             return cache_memory[args]
-        else:
-            print("Calculating new result")
-            result = func(*args)
-            cache_memory[args] = result
-            return result
+
+        print("Calculating new result")
+        result = func(*args)
+        cache_memory[args] = result
+        return result
     return wrapper
